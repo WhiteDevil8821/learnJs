42b7b235f17e187a391f899793e64e232d00b34a
WhiteDevil8821 <ankit.kumar221099@gmail.com>
2025-12-07 20:20:17 +0530

    Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12736576
+let accountEmail = "ankit@test.com"
+var accountPassword = "12345"
+accountCity = "Ranchi"
+
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "dfjgdshrg@test.com"
+accountPassword = "21212121"
+awccountCity = "Bangalore"
+
+console.log(accountId)
+
+/*
+Prefer not to use var
+bnecause of issue in block scope and function scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

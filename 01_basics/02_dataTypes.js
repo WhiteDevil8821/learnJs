42fc8e9068903bb8c70aaf451f1f6366190c8bee
WhiteDevil8821 <ankit.kumar221099@gmail.com>
2025-12-07 20:38:48 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  // treat all JS code as newer version
+
+// alert("Hello World!");  // we are using node js not browser
+
+console.log(3 + 3); // code readability sho8ld be high
+
+console.log("Hello World!");
+
+
+let name = "ankit"
+let age = 27
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53 -1
+// bigint
+// string => "" , '' , ``
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
